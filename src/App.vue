37af7a21cbf61b3371d68a81396de9b53feacbe8
Matheus Henrique Julidori <matheus_julidori@yahoo.com.br>
2021-01-18 14:45:14 -0300

    Módulos 4 e 5

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,49 @@
 <template>
   <div class="corpo">
     <h1 class="titulo">{{titulo}}</h1>
+
+    <input type="search" class="filtro" @input="filtro = $event.target.value" placeholder="Procure uma foto">
     <ul class="lista-fotos">
-      <li class="lista-fotos-item" v-for="foto of fotos" :key="foto">
-        <div class="painel">
-          <h2 class="painel-titulo">{{ foto.titulo}}</h2>
-          <div class="painel-conteudo">
-            <img class="imagem-responsiva" :src="foto.url" :alt="foto.alt">
-          </div>
-        </div> 
+      <li class="lista-fotos-item" v-for="foto of fotosComFiltro" :key="foto">
+
+        <meu-painel :titulo="foto.titulo">
+          <imagem-responsiva :url="foto.url" :titulo="foto.titulo"/>
+        </meu-painel>
+
       </li>
     </ul>
   </div>
 </template>
 
 <script>
+
+import Painel from './components/shared/painel/Painel.vue';
+import ImagemResponsiva from './components/shared/imagem-responsiva/ImagemResponsiva.vue';
+
 export default {
+
+  components:{
+    'meu-painel': Painel,
+    'imagem-responsiva': ImagemResponsiva
+  },
 
   data () {
     return {
       titulo: 'Alurapic',
-      fotos: []
+      fotos: [],
+      filtro: ''
+    }
+  },
+
+  computed:{
+    fotosComFiltro(){
+      if(this.filtro){
+        let exp = new RegExp(this.filtro.trim(), 'i');
+        return this.fotos.filter(foto => exp.test(foto.titulo));
+      }
+      else{
+        return this.fotos;
+      }
     }
   },
 
@@ -40,6 +63,11 @@ export default {
     text-align: center;
   }
 
+  .filtro {
+    display:block;
+    width:100%;
+  }
+
   .corpo {
     font-family: Helvetica, sans-serif;
     margin: 0 auto;
@@ -56,28 +84,5 @@ export default {
 
   .imagem-responsiva {
     width: 100%;
-  }
-
-  /* estilo do painel */ 
-
-   .painel {
-    padding: 0 auto;
-    border: solid 2px grey;
-    display: inline-block;
-    margin: 5px;
-    box-shadow: 5px 5px 10px grey;
-    width: 200px;
-    height: 100%;
-    vertical-align: top;
-    text-align: center;
-  }
-
-  .painel .painel-titulo {
-    text-align: center;
-    border: solid 2px;
-    background: lightblue;
-    margin: 0 0 15px 0;
-    padding: 10px;
-    text-transform: uppercase;
   }
 </style>
--- src/components/shared/painel/Painel.vue
+++ src/components/shared/painel/Painel.vue
@@ -1,37 +1,61 @@
 <template>
-    <div class="painel">
-        <h2 class="painel-titulo"></h2>
-        <div class="painel-conteudo">
-    </div>
+  <div class="painel">
+    <h2 class="painel-titulo" @dblclick="visivel = !visivel">{{ titulo }}</h2>
+    <transition name="painel-fade">
+      <div class="painel-conteudo" v-show="visivel">
+        <slot>
+          <!--Slot faz com que todos os elementos digitados dentro do painel no App.vue sejam colocados aqui dentro-->
+        </slot>
+      </div>
+    </transition>
+  </div>
 </template>
 
 <script>
 export default {
+  props: ["titulo"],
 
-}
+  data() {
+    return {
+      visivel: true,
+    };
+  },
+};
 </script>
 
-<style>
-  /* estilo do painel */ 
+<style scoped>
+/* estilo do painel ESCOPO LOCAL POR CAUSA DO SCOPED*/
 
-   .painel {
-    padding: 0 auto;
-    border: solid 2px grey;
-    display: inline-block;
-    margin: 5px;
-    box-shadow: 5px 5px 10px grey;
-    width: 200px;
-    height: 100%;
-    vertical-align: top;
-    text-align: center;
-  }
+.painel {
+  padding: 0 auto;
+  border: solid 2px grey;
+  display: inline-block;
+  margin: 5px;
+  box-shadow: 5px 5px 10px grey;
+  width: 200px;
+  height: 100%;
+  vertical-align: top;
+  text-align: center;
+}
 
-  .painel .painel-titulo {
-    text-align: center;
-    border: solid 2px;
-    background: lightblue;
-    margin: 0 0 15px 0;
-    padding: 10px;
-    text-transform: uppercase;
-  }
+.painel .painel-titulo {
+  text-align: center;
+  border: solid 2px;
+  background: lightblue;
+  margin: 0 0 15px 0;
+  padding: 10px;
+  text-transform: uppercase;
+}
+
+* {
+  box-shadow: 5px 5px 5px;
+}
+
+.painel-fade-enter, .painel-fade-leave-active {
+  opacity: 0
+}
+
+.painel-fade-enter-active, .painel-fade-leave-active {
+  transition: opacity .4s
+}
 </style>
